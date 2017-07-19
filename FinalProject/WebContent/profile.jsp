7d039b0dfd27726ee1a7552bf1e17aa9a9ef4a8b
soclic <ikimi321@naver.com>
2017-07-19 10:10:18 +0900

    test999999
    99999

---
+++ FinalProject/WebContent/profile.jsp
@@ -0,0 +1,94 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    <%@ include file="menu.jsp" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+	<head>
+	<script type="text/javascript">
+		$(document).ready(function(){
+			$('#profileUpdate').click(function(){
+				
+				
+			});
+			
+		});
+	
+	</script>
+	
+	</head>
+	<body>
+	<style>
+	.photo{
+		position: relative;
+		float: left;
+		margin-right: 30px;
+		width: 300px;
+		height: 300px;
+		
+	}
+	
+	</style>
+	<div id="fh5co-main">
+		<div class="container">
+			<div class="row">
+			
+				<div class="col-md-8 col-md-offset-2">
+					<h2>프로필 관리</h2>
+					<img src="<c:url value="/profile/${member.id}"/>/${member.photo}" alt="Free HTML5 template by FREEHTML5.co" class="img-rounded img-responsive photo">
+					
+					<p>아이디 : ${member.id}</p>
+					<p>닉네임 : ${member.nickName}</p>
+					<p>사진 : ${member.photo==null?"사진 없음":member.photo}</p>
+					<p>포인트 : <fmt:formatNumber value="${member.balance}" type="number"/> 포인트</p>
+					<p>관리자 : ${member.admin}</p>
+					
+					<p>관심분야 :</p>
+					
+					<p>판매완료 : 00건</p>
+					<p>구매완료 : 00건</p>
+					<p>판매평점 : 00점</p>
+					<div class="fh5co-spacer fh5co-spacer-sm"></div>
+					<p>판매 가능 재능 목록</p>
+					<a data-toggle="modal" href="#profileModal" class="btn btn-sm btn-info">정보 수정</a>
+					<c:if test="${member.admin==1}">
+						<a href="memberList.jsp" class="btn btn-sm btn-info">회원관리</a>
+					</c:if>
+					
+				</div>
+        	</div>
+       </div>
+	</div>
+
+	<!-- 프로필 수정  Modal -->
+	<div class="modal fade" id="profileModal" role="dialog">
+		<div class="modal-dialog">
+
+			<!-- Modal content-->
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal">&times;</button>
+					<h4 class="modal-title">정보 수정</h4>
+				</div>
+				<div class="modal-body">
+					<form action="profileUpdate.do" method="post" enctype="multipart/form-data">
+					<table id="profileTable">
+						<tr>
+							<td>사진 : </td><td><input type="file" name="file"></td>
+						</tr>
+						<tr>
+							<td>관심 분야 : </td><td>관심분야 선택</td>
+						</tr>
+					</table>
+					<input type="submit" value="수정" class="btn-sm btn-info">
+					</form>
+				</div>
+			</div>
+
+		</div>
+	</div>
+	<!-- Modal -->
+
+
+
+</body>
+</html>
---
+++ FinalProject/src/controller/boardController.java
@@ -0,0 +1,121 @@
+package controller;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import model.Board;
+import model.Member;
+import service.BoardService;
+
+
+@Controller
+public class boardController{
+	
+	@Autowired
+	private BoardService boardService;
+	
+	/**
+	 * 검색 api
+	 * */
+	@RequestMapping("searchAddr.do")
+	public void mapSearch(HttpServletRequest req, HttpServletResponse resp){
+		resp.setCharacterEncoding("UTF-8");
+		resp.setContentType("text/html; charset=UTF-8");
+		try {
+			PrintWriter pw = resp.getWriter();
+			
+			System.out.println("searchAddr 하러 왔어");
+			String inputAddr = req.getParameter("inputAddr");
+			System.out.println("검색하려는 주소 : " + inputAddr);
+			
+			//주소검색 시작
+			String clientId = "yWnmGXGFAVlHFUidS2ri";//애플리케이션 클라이언트 아이디값";
+	        String clientSecret = "x1szaIQMgv";//애플리케이션 클라이언트 시크릿값";
+	        
+	        
+	        String text = URLEncoder.encode(inputAddr, "UTF-8");
+            String apiURL = "https://openapi.naver.com/v1/search/local.json?query="+ text; // json 결과
+            //String apiURL = "https://openapi.naver.com/v1/search/blog.xml?query="+ text; // xml 결과
+            URL url = new URL(apiURL);
+            HttpURLConnection con = (HttpURLConnection)url.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("X-Naver-Client-Id", clientId);
+            con.setRequestProperty("X-Naver-Client-Secret", clientSecret);
+            int responseCode = con.getResponseCode();
+            BufferedReader br;
+            if(responseCode==200) { // 정상 호출
+                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            } else {  // 에러 발생
+                br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+            }
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = br.readLine()) != null) {
+                response.append(inputLine);
+            }
+            br.close();
+            System.out.println(response.toString());
+            System.out.println("테스트  " + response);
+           
+            pw.println(response);
+	        
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+
+	//////////////////////////////////////////////////////////////////////////////////////////////////
+	@RequestMapping("boardInsert.do")
+	public void board(HttpServletRequest req, HttpServletResponse resp, HttpSession session) {
+		
+		int major = Integer.parseInt(req.getParameter("major"));
+		int minor = Integer.parseInt(req.getParameter("minor"));
+		String title = req.getParameter("title");
+		String end_date = req.getParameter("end_date");
+		int limit = Integer.parseInt(req.getParameter("limit"));
+		String address = req.getParameter("addrResult");
+		String address2 = req.getParameter("addrResult2");
+		int price = Integer.parseInt(req.getParameter("price"));
+		int optionprice = Integer.parseInt(req.getParameter("optionprice"));
+		String content = req.getParameter("content");
+		System.out.println("major ="+major+" / minor ="+minor);
+		System.out.println(address);
+		System.out.println(address2);
+		System.out.println("제목" + title);
+		System.out.println("마감일" + end_date);
+		
+		String id = ((Member)session.getAttribute("member")).getId();
+		System.out.println("id찍어볼거야 = "+ id);
+		
+		Board board = new Board(major, minor, title, id, content, new Date(), end_date, limit, 0, price, optionprice, 0, 0, 0, 0);
+		
+		boardService.insertBoard(board);
+		
+	}
+
+
+	
+
+
+}
