f683ae1b358cd8a59a757b3adec3082d49dc851b
ykson83 <ykson8300@gmail.com>
2017-07-19 10:26:03 +0900

    map mapper썼어!!

--- FinalProject/src/service/BoardService.java
+++ FinalProject/src/service/BoardService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import dao.BoardDao;
 import model.Board;
+import model.MapInfo;
 
 @Service
 public class BoardService {
@@ -14,6 +15,9 @@ public class BoardService {
 	@Autowired
 	private BoardDao boardDao;
 	
+	/**
+	 * table명 : board
+	 * */
 	public boolean insertBoard(Board board){
 		return boardDao.insertBoard(board);
 	}
@@ -26,12 +30,32 @@ public class BoardService {
 		return boardDao.deleteBoard(no);
 	}
 	
-	public Board selectOne(int no){
-		return boardDao.selectOne(no);
+	public Board selectOneBoard(int no){
+		return boardDao.selectOneBoard(no);
 	}
 	
-	public List<Board>selectAll(){
-		return boardDao.selectAll();
+	public List<Board>selectAllBoard(){
+		return boardDao.selectAllBoard();
+	}
+	
+	
+	/**
+	 * table명 : map
+	 * */
+	public boolean insertMap(MapInfo map){
+		return boardDao.insertMap(map);
+	}
+	
+	public boolean updateMap(MapInfo map){
+		return boardDao.updateMap(map);
+	}
+	
+	public boolean deleteMap(int no){
+		return boardDao.deleteMap(no);
+	}
+	
+	public MapInfo selectOneMap(int no){
+		return boardDao.selectOneMap(no);
 	}
 		
 
